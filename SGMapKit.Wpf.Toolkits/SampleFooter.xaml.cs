dfef52ab85ba641a3786aecb5930be27cde05dd4
SlimGIS_dev <dev@slimgis.com>
2017-05-06 15:15:28 +0800

    Init porject.

---
+++ SGMapKit.Wpf.Toolkits/SampleFooter.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SlimGis.MapKit.Wpf.Toolkits
+{
+    /// <summary>
+    /// Interaction logic for Footer.xaml
+    /// </summary>
+    public partial class SampleFooter : Control
+    {
+        public SampleFooter()
+        {
+            InitializeComponent();
+        }
+
+        private void QuestionButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("http://forums.slimgis.com/");
+        }
+
+        private void ContactButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Application.Current.MainWindow, "Please send email to \"support@slimgis.com\".", "Contact us", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("http://www.slimgis.com/");
+        }
+
+        private void LearnMoreButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("http://blog.slimgis.com/");
+        }
+    }
+}
---
+++ SGMapKit.Wpf.Toolkits/SampleHeader.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SlimGis.MapKit.Wpf.Toolkits
+{
+    /// <summary>
+    /// Interaction logic for SampleHeader.xaml
+    /// </summary>
+    public partial class SampleHeader : Control
+    {
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register("Source", typeof(ImageSource), typeof(SampleHeader), new PropertyMetadata(new BitmapImage(new Uri("pack://application:,,,/SGMapKit.Wpf.Toolkits;Component/Images/SlimGIS-Logo-40.png"))));
+
+        public SampleHeader()
+        {
+            InitializeComponent();
+        }
+
+        public ImageSource Source
+        {
+            get { return (ImageSource)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+    }
+}
