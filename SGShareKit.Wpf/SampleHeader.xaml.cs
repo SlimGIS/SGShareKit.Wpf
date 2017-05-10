b5a834e56187c8e0ab2e272ab2517356745b4d3d
SlimGIS_dev <dev@slimgis.com>
2017-05-10 22:01:00 +0800

    Add title property to SampleHeader.

--- SGShareKit.Wpf/SampleHeader.xaml.cs
+++ SGShareKit.Wpf/SampleHeader.xaml.cs
@@ -10,13 +10,22 @@ namespace SlimGis.Sharedkits.Wpf
     /// Interaction logic for SampleHeader.xaml
     /// </summary>
     public partial class SampleHeader : Control
-    {
+    {        
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(SampleHeader), new PropertyMetadata("SlimGIS MapKit Samples for WPF"));
+
         public static readonly DependencyProperty SourceProperty =
             DependencyProperty.Register("Source", typeof(ImageSource), typeof(SampleHeader), new PropertyMetadata(new BitmapImage(new Uri("pack://application:,,,/SGShareKit.Wpf;Component/Images/SlimGIS-Logo-40.png", UriKind.RelativeOrAbsolute))));
 
         public SampleHeader()
         {
             InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
         }
 
         public ImageSource Source
